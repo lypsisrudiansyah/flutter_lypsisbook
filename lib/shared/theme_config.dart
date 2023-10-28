bb00d7ac285e6896f78be7470744b5fef1b250f3
lypsisrudiansyah <rudiansyahether@gmail.com>
2023-10-28 20:52:23 +0700

    update theme_config.dart

--- lib/shared/theme_config.dart
+++ lib/shared/theme_config.dart
@@ -3,3 +3,4 @@ import 'package:flutter/material.dart';
 const primaryColor = Color(0xFFC12FFF);
 const disabledColor = Color(0xFFCDCDCD);
 const disabledTextColor = Color(0xFF455154);
+const secondaryColor = Color(0xFF969FA2);
