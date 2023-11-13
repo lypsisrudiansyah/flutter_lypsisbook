99af58736540ea64489709c3fb7296a8ba84456f
Rudiansyah <rudiansyahether@gmail.com>
2023-11-13 21:14:22 +0700

    🎨 style(dashboard_view.dart): refactor card layout to improve readability and maintainability

--- lib/module/dashboard/view/dashboard_view.dart
+++ lib/module/dashboard/view/dashboard_view.dart
@@ -229,15 +229,112 @@ class DashboardView extends StatefulWidget {
                 physics: const ScrollPhysics(),
                 itemBuilder: (BuildContext context, int index) {
                   return Card(
-                    child: ListTile(
-                      leading: CircleAvatar(
-                        backgroundColor: Colors.grey[200],
-                        backgroundImage: const NetworkImage(
-                          "https://i.ibb.co/QrTHd59/woman.jpg",
-                        ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(12.0),
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Container(
+                            width: 90,
+                            height: 90,
+                            margin: const EdgeInsets.only(right: 10.0),
+                            decoration: BoxDecoration(
+                              image: const DecorationImage(
+                                image: NetworkImage(
+                                  "https://images.unsplash.com/photo-1528735602780-2552fd46c7af?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1173&q=80",
+                                ),
+                                fit: BoxFit.cover,
+                              ),
+                              borderRadius: const BorderRadius.all(
+                                Radius.circular(16.0),
+                              ),
+                              color: Colors.blue[400],
+                            ),
+                            child: Stack(
+                              children: [
+                                Container(
+                                  padding: const EdgeInsets.all(6.0),
+                                  margin: const EdgeInsets.all(8.0),
+                                  decoration: BoxDecoration(
+                                    color: Colors.green[800],
+                                    borderRadius: const BorderRadius.all(
+                                      Radius.circular(
+                                        12.0,
+                                      ),
+                                    ),
+                                  ),
+                                  child: const Text(
+                                    "PROMO",
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 8.0,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: [
+                                const Text(
+                                  "Roti bakar Cimanggis",
+                                  style: TextStyle(
+                                    fontSize: 14.0,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                const SizedBox(height: 6.0),
+                                Row(
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  children: [
+                                    const Text(
+                                      "8.1 km",
+                                      style: TextStyle(
+                                        fontSize: 10.0,
+                                      ),
+                                    ),
+                                    const SizedBox(width: 4.0),
+                                    const Icon(
+                                      Icons.circle,
+                                      size: 4.0,
+                                    ),
+                                    const SizedBox(width: 4.0),
+                                    Icon(
+                                      Icons.star,
+                                      color: Colors.orange[400],
+                                      size: 16.0,
+                                    ),
+                                    const Text(
+                                      "4.8",
+                                      style: TextStyle(
+                                        fontSize: 10.0,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                const SizedBox(height: 6.0),
+                                const Text(
+                                  "Bakery & Cake . Breakfast . Snack",
+                                  style: TextStyle(
+                                    fontSize: 10.0,
+                                  ),
+                                ),
+                                const SizedBox(height: 6.0),
+                                const Text(
+                                  "€24",
+                                  style: TextStyle(
+                                    fontSize: 14.0,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
                       ),
-                      title: const Text("Jessica Doe"),
-                      subtitle: const Text("Programmer"),
                     ),
                   );
                 },
