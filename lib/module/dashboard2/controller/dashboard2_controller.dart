b5d4613976688520e913c61abbe27b186c9800d6
Rudiansyah <rudiansyahether@gmail.com>
2023-11-14 22:55:56 +0700

    🎉 feat(dashboard2): add Dashboard2Controller and Dashboard2View
    ✨ feat(dashboard2_controller.dart): create Dashboard2Controller class to handle the logic and state of the Dashboard2View
    ✨ feat(dashboard2_view.dart): create Dashboard2View class to display the dashboard UI and interact with the Dashboard2Controller
    
    These files are added to introduce a new dashboard feature in the application. The Dashboard2Controller class is responsible for managing the state and logic of the dashboard, while the Dashboard2View class is responsible for rendering the UI and interacting with the controller.

---
+++ lib/module/dashboard2/controller/dashboard2_controller.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_lypsisbook/core.dart';
+import '../view/dashboard_view.dart';
+
+class Dashboard2Controller extends State<Dashboard2View> {
+  static late Dashboard2Controller instance;
+  late Dashboard2View view;
+
+  @override
+  void initState() {
+    instance = this;
+    super.initState();
+  }
+
+  @override
+  void dispose() => super.dispose();
+
+  @override
+  Widget build(BuildContext context) => widget.build(context, this);
+}
---
+++ lib/module/dashboard2/view/dashboard2_view.dart
@@ -0,0 +1,351 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_lypsisbook/core.dart';
+import '../controller/dashboard_controller.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class Dashboard2View extends StatefulWidget {
+  const Dashboard2View({Key? key}) : super(key: key);
+
+  Widget build(context, Dashboard2Controller controller) {
+    controller.view = this;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Dashboard"),
+        actions: const [],
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: const EdgeInsets.all(15.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              // * Widget Search
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(8.0),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      blurRadius: 5.0,
+                      offset: const Offset(0, 3),
+                    ),
+                  ],
+                ),
+                child: TextField(
+                  decoration: InputDecoration(
+                    hintText: 'Search',
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8.0),
+                      borderSide: BorderSide(
+                        color: Colors.grey[300]!,
+                      ),
+                    ),
+                    prefixIcon: Icon(
+                      Icons.search,
+                      color: Colors.blueGrey[900],
+                    ),
+                    suffixIcon: Container(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Icon(
+                        Icons.sort,
+                        color: Colors.blueGrey[900],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 10),
+              // * Widget Banner
+              Container(
+                height: 160.0,
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                    image: NetworkImage(
+                      "https://images.unsplash.com/photo-1550547660-d9450f859349?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=765&q=80",
+                    ),
+                    fit: BoxFit.cover,
+                  ),
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(
+                      16.0,
+                    ),
+                  ),
+                ),
+                child: Stack(
+                  children: [
+                    Container(
+                      decoration: const BoxDecoration(
+                        color: Colors.black26,
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(
+                            16.0,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      left: 20.0,
+                      top: 0.0,
+                      bottom: 0.0,
+                      child: SizedBox(
+                        width: 100.0,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              "30%",
+                              style: GoogleFonts.oswald(
+                                fontSize: 30.0,
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Text(
+                              "Discount Only Valid for Today",
+                              style: GoogleFonts.oswald(
+                                fontSize: 16.0,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 10),
+              // * Widget Menu
+              LayoutBuilder(
+                builder: (context, constraint) {
+                  List menus = [
+                    {
+                      "icon": "https://cdn-icons-png.flaticon.com/128/878/878052.png",
+                      "label": "Burger",
+                      "onTap": () {},
+                    },
+                    {
+                      "icon": "https://cdn-icons-png.flaticon.com/128/3595/3595455.png",
+                      "label": "Pizza",
+                      "onTap": () {},
+                    },
+                    {
+                      "icon": "https://cdn-icons-png.flaticon.com/128/2718/2718224.png",
+                      "label": "Noodles",
+                      "onTap": () {},
+                    },
+                    {
+                      "icon": "https://cdn-icons-png.flaticon.com/128/8060/8060549.png",
+                      "label": "Meat",
+                      "onTap": () {},
+                    },
+                    {
+                      "icon": "https://cdn-icons-png.flaticon.com/128/454/454570.png",
+                      "label": "Soup",
+                      "onTap": () {},
+                    },
+                    {
+                      "icon": "https://cdn-icons-png.flaticon.com/128/2965/2965567.png",
+                      "label": "Dessert",
+                      "onTap": () {},
+                    },
+                    {
+                      "icon": "https://cdn-icons-png.flaticon.com/128/2769/2769608.png",
+                      "label": "Drink",
+                      "onTap": () {},
+                    },
+                    {
+                      "icon": "https://cdn-icons-png.flaticon.com/128/1037/1037855.png",
+                      "label": "Others",
+                      "onTap": () {},
+                    },
+                  ];
+
+                  return Wrap(
+                    children: List.generate(
+                      menus.length,
+                      (index) {
+                        var item = menus[index];
+
+                        var size = constraint.biggest.width / 4;
+
+                        return SizedBox(
+                          width: size,
+                          height: size,
+                          child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              foregroundColor: Colors.blueGrey,
+                              animationDuration: const Duration(milliseconds: 1000),
+                              backgroundColor: Colors.transparent,
+                              splashFactory: InkSplash.splashFactory,
+                              shadowColor: Colors.transparent,
+                              elevation: 0.0,
+                            ),
+                            onPressed: () => item["onTap"](),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Image.network(
+                                  item["icon"],
+                                  width: 30.0,
+                                ),
+                                const SizedBox(
+                                  height: 6.0,
+                                ),
+                                Text(
+                                  "${item["label"]}",
+                                  textAlign: TextAlign.center,
+                                  overflow: TextOverflow.ellipsis,
+                                  style: const TextStyle(
+                                    fontSize: 11.0,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+                  );
+                },
+              ),
+              const SizedBox(height: 10),
+              const Text(
+                "Products",
+                style: TextStyle(
+                  fontSize: 25.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 12),
+              ListView.builder(
+                itemCount: 3,
+                shrinkWrap: true,
+                physics: const ScrollPhysics(),
+                itemBuilder: (BuildContext context, int index) {
+                  return Card(
+                    child: Padding(
+                      padding: const EdgeInsets.all(12.0),
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Container(
+                            width: 90,
+                            height: 90,
+                            margin: const EdgeInsets.only(right: 10.0),
+                            decoration: BoxDecoration(
+                              image: const DecorationImage(
+                                image: NetworkImage(
+                                  "https://images.unsplash.com/photo-1528735602780-2552fd46c7af?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1173&q=80",
+                                ),
+                                fit: BoxFit.cover,
+                              ),
+                              borderRadius: const BorderRadius.all(
+                                Radius.circular(16.0),
+                              ),
+                              color: Colors.blue[400],
+                            ),
+                            child: Stack(
+                              children: [
+                                Container(
+                                  padding: const EdgeInsets.all(6.0),
+                                  margin: const EdgeInsets.all(8.0),
+                                  decoration: BoxDecoration(
+                                    color: Colors.green[800],
+                                    borderRadius: const BorderRadius.all(
+                                      Radius.circular(
+                                        12.0,
+                                      ),
+                                    ),
+                                  ),
+                                  child: const Text(
+                                    "PROMO",
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 8.0,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: [
+                                const Text(
+                                  "Roti bakar Cimanggis",
+                                  style: TextStyle(
+                                    fontSize: 14.0,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                const SizedBox(height: 6.0),
+                                Row(
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  children: [
+                                    const Text(
+                                      "8.1 km",
+                                      style: TextStyle(
+                                        fontSize: 10.0,
+                                      ),
+                                    ),
+                                    const SizedBox(width: 4.0),
+                                    const Icon(
+                                      Icons.circle,
+                                      size: 4.0,
+                                    ),
+                                    const SizedBox(width: 4.0),
+                                    Icon(
+                                      Icons.star,
+                                      color: Colors.orange[400],
+                                      size: 16.0,
+                                    ),
+                                    const Text(
+                                      "4.8",
+                                      style: TextStyle(
+                                        fontSize: 10.0,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                const SizedBox(height: 6.0),
+                                const Text(
+                                  "Bakery & Cake . Breakfast . Snack",
+                                  style: TextStyle(
+                                    fontSize: 10.0,
+                                  ),
+                                ),
+                                const SizedBox(height: 6.0),
+                                const Text(
+                                  "€24",
+                                  style: TextStyle(
+                                    fontSize: 14.0,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  State<Dashboard2View> createState() => Dashboard2Controller();
+}
