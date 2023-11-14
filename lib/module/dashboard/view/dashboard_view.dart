fa2b21ae2bb0635848ab3087280ef2a7c66ac229
Rudiansyah <rudiansyahether@gmail.com>
2023-11-15 00:30:24 +0700

    🎨 style(dashboard_view.dart): improve code formatting and readability ✨ feat(dashboard_view.dart): add carousel slider to display images ✨ feat(dashboard_view.dart): add app logo and title to the app bar ✨ feat(dashboard_view.dart): add categories section with see all text ✨ feat(dashboard_view.dart): add grid view for categories ✨ feat(dashboard_view.dart): add flash sale section with see all text ✨ feat(dashboard_view.dart): add grid view for products in flash sale section 🐛 fix(dashboard_view.dart): fix indentation and remove unused imports

--- lib/module/dashboard/view/dashboard_view.dart
+++ lib/module/dashboard/view/dashboard_view.dart
@@ -1,31 +1,287 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter_lypsisbook/core.dart';
 import '../controller/dashboard_controller.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
 class DashboardView extends StatefulWidget {
-    const DashboardView({Key? key}) : super(key: key);
+  const DashboardView({Key? key}) : super(key: key);
 
-    Widget build(context, DashboardController controller) {
+  Widget build(context, DashboardController controller) {
     controller.view = this;
 
     return Scaffold(
-        appBar: AppBar(
-        title: const Text("Dashboard"),
-        actions: const [],
-        ),
-        body: SingleChildScrollView(
-        child: Container(
-            padding: const EdgeInsets.all(10.0),
-            child: Column(
-            children: const [],
+      appBar: AppBar(
+        title: Row(
+          children: [
+            Image.asset(
+              'assets/images/lypsisbook-480.png',
+              width: 32.0,
+              height: 32.0,
+              fit: BoxFit.fill,
             ),
+            const SizedBox(width: 4),
+            const Text(
+              "Lypsis-Stores",
+              style: TextStyle(
+                fontSize: 16.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
         ),
-        ),
-    );
-    }
+        actions: const [],
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+            children: [
+              // * The Carousel Slider
+              Builder(builder: (context) {
+                List images = [
+                  "https://images.unsplash.com/photo-1555939594-58d7cb561ad1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+                  "https://images.unsplash.com/photo-1540189549336-e6e99c3679fe?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+                  "https://images.unsplash.com/photo-1565299624946-b28f40a0ae38?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=781&q=80",
+                  "https://images.unsplash.com/photo-1565958011703-44f9829ba187?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=765&q=80",
+                  "https://images.unsplash.com/photo-1482049016688-2d3e1b311543?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=710&q=80",
+                ];
 
-    @override
-    State<DashboardView> createState() => DashboardController();
+                return CarouselSlider(
+                  options: CarouselOptions(
+                    height: 90.0,
+                    autoPlay: true,
+                    enlargeCenterPage: true,
+                  ),
+                  items: images.map((imageUrl) {
+                    return Builder(
+                      builder: (BuildContext context) {
+                        return Container(
+                          width: MediaQuery.of(context).size.width,
+                          margin: const EdgeInsets.symmetric(horizontal: 5.0),
+                          decoration: BoxDecoration(
+                            color: Colors.amber,
+                            borderRadius: const BorderRadius.all(
+                              Radius.circular(6.0),
+                            ),
+                            image: DecorationImage(
+                              image: NetworkImage(
+                                imageUrl,
+                              ),
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        );
+                      },
+                    );
+                  }).toList(),
+                );
+              }),
+              const SizedBox(height: 16),
+              // * Text Categories  With See All Text
+              ListTile(
+                title: const Text(
+                  "Categories",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                contentPadding: EdgeInsets.zero,
+                trailing: SizedBox(
+                  // color: Colors.grey[200],
+                  width: 100,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: const [
+                      Text(
+                        "See All",
+                        style: TextStyle(
+                          fontSize: 12.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(width: 4),
+                      Icon(
+                        Icons.chevron_right,
+                        size: 24.0,
+                        color: Colors.black,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              // * GridView Categories
+              GridView.builder(
+                padding: EdgeInsets.zero,
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  childAspectRatio: 1.0 / 0.3,
+                  crossAxisCount: 2,
+                  mainAxisSpacing: 8,
+                  crossAxisSpacing: 8,
+                ),
+                itemCount: controller.categories.length,
+                shrinkWrap: true,
+                physics: const ScrollPhysics(),
+                itemBuilder: (BuildContext context, int index) {
+                  var itemCategory = controller.categories[index];
+                  return Container(
+                    decoration: const BoxDecoration(
+                      color: Colors.black87,
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(8.0),
+                      ),
+                    ),
+                    child: Padding(
+                      // symetric: horizoltal
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 20.0,
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            itemCategory,
+                            style: const TextStyle(
+                              fontSize: 16.0,
+                              color: Colors.white,
+                            ),
+                          ),
+                          const Icon(
+                            Icons.chevron_right,
+                            size: 24.0,
+                            color: Colors.white,
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              ),
+              // * Text Flash Sale With See All Text
+              ListTile(
+                title: const Text(
+                  "Flash Sale",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                contentPadding: EdgeInsets.zero,
+                trailing: SizedBox(
+                  // color: Colors.grey[200],
+                  width: 100,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: const [
+                      Text(
+                        "See All",
+                        style: TextStyle(
+                          fontSize: 12.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(width: 4),
+                      Icon(
+                        Icons.chevron_right,
+                        size: 24.0,
+                        color: Colors.black,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              GridView.builder(
+                padding: EdgeInsets.zero,
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  childAspectRatio: 1.0 / 1.4,
+                  crossAxisCount: 2,
+                  mainAxisSpacing: 8,
+                  crossAxisSpacing: 8,
+                ),
+                itemCount: controller.products.length,
+                shrinkWrap: true,
+                physics: const ScrollPhysics(),
+                itemBuilder: (BuildContext context, int index) {
+                  var item = controller.products[index];
+                  return Container(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Expanded(
+                          child: Container(
+                            width: MediaQuery.of(context).size.width,
+                            clipBehavior: Clip.antiAlias,
+                            decoration: BoxDecoration(
+                              image: DecorationImage(
+                                image: NetworkImage(
+                                  item["photo"],
+                                ),
+                                fit: BoxFit.cover,
+                              ),
+                              borderRadius: const BorderRadius.all(
+                                Radius.circular(
+                                  6.0,
+                                ),
+                              ),
+                            ),
+                            child: Stack(
+                              children: [
+                                Positioned(
+                                  right: 6.0,
+                                  top: 8.0,
+                                  child: CircleAvatar(
+                                    radius: 14.0,
+                                    backgroundColor: Colors.white,
+                                    child: Icon(
+                                      Icons.favorite,
+                                      color: item['is_favorite'] == true ? Colors.red : Colors.black87,
+                                      size: 14.0,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 8.0,
+                        ),
+                        Text(
+                          item["product_name"],
+                          style: const TextStyle(
+                            fontSize: 16.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 4.0,
+                        ),
+                        Text(
+                          item["category"],
+                          style: const TextStyle(
+                            fontSize: 12.0,
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 4.0,
+                        ),
+                        Text(
+                          "${item["price"]}",
+                          style: const TextStyle(
+                            fontSize: 16.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                  );
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  State<DashboardView> createState() => DashboardController();
 }
-    
