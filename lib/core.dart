2f7b97b65802a420427c9e3bea8af59ef6816213
Rudiansyah <rudiansyahether@gmail.com>
2023-11-14 22:55:09 +0700

    ✨ feat(config.json): add new modules to the list of modules to be loaded in the app ✨ feat(core.dart): add new module imports for dashboard2, edit_profile, product_detail, and product_list to be used in the app

--- lib/core.dart
+++ lib/core.dart
@@ -7,6 +7,10 @@ export 'package:flutter_lypsisbook/module/change_password/controller/change_pass
 export 'package:flutter_lypsisbook/module/change_password/view/change_password_view.dart';
 export 'package:flutter_lypsisbook/module/dashboard/controller/dashboard_controller.dart';
 export 'package:flutter_lypsisbook/module/dashboard/view/dashboard_view.dart';
+export 'package:flutter_lypsisbook/module/dashboard2/controller/dashboard_controller.dart';
+export 'package:flutter_lypsisbook/module/dashboard2/view/dashboard_view.dart';
+export 'package:flutter_lypsisbook/module/edit_profile/controller/edit_profile_controller.dart';
+export 'package:flutter_lypsisbook/module/edit_profile/view/edit_profile_view.dart';
 export 'package:flutter_lypsisbook/module/forgot_password/controller/forgot_password_controller.dart';
 export 'package:flutter_lypsisbook/module/forgot_password/view/forgot_password_view.dart';
 export 'package:flutter_lypsisbook/module/intro/controller/intro_controller.dart';
@@ -15,6 +19,10 @@ export 'package:flutter_lypsisbook/module/login/controller/login_controller.dart
 export 'package:flutter_lypsisbook/module/login/view/login_view.dart';
 export 'package:flutter_lypsisbook/module/main_navigation/controller/main_navigation_controller.dart';
 export 'package:flutter_lypsisbook/module/main_navigation/view/main_navigation_view.dart';
+export 'package:flutter_lypsisbook/module/product_detail/controller/product_detail_controller.dart';
+export 'package:flutter_lypsisbook/module/product_detail/view/product_detail_view.dart';
+export 'package:flutter_lypsisbook/module/product_list/controller/product_list_controller.dart';
+export 'package:flutter_lypsisbook/module/product_list/view/product_list_view.dart';
 export 'package:flutter_lypsisbook/module/profile/controller/profile_controller.dart';
 export 'package:flutter_lypsisbook/module/profile/view/profile_view.dart';
 export 'package:flutter_lypsisbook/module/register/controller/register_controller.dart';
