16b8f3b4acc652ef4424eb9288c39290632ee67c
Rudiansyah <rudiansyahether@gmail.com>
2023-11-15 22:47:04 +0700

    🐛 fix(product_list_view.dart): change app bar title to a column with "Flash Sale" and "15.3K items" to improve UI ✨ feat(product_list_view.dart): add sort by button to the app bar for future functionality ✨ feat(product_list_view.dart): add filter button to the app bar for future functionality ✨ feat(product_list_view.dart): update grid item layout to improve UI and add gradient overlay ✨ feat(product_list_view.dart): add text fields to grid items to display product information
    🔧 chore(product_list_controller.dart): update product list data to include additional products

--- lib/module/product_list/view/product_list_view.dart
+++ lib/module/product_list/view/product_list_view.dart
@@ -10,7 +10,24 @@ class ProductListView extends StatefulWidget {
 
     return Scaffold(
       appBar: AppBar(
-        title: const Text("ProductList"),
+        title: Column(
+          children: const [
+            Text(
+              "Flash Fale",
+              style: TextStyle(
+                fontSize: 16.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              "15.3K items",
+              style: TextStyle(
+                fontSize: 12.0,
+              ),
+            ),
+          ],
+        ),
+        centerTitle: true,
         actions: const [],
       ),
       body: SingleChildScrollView(
@@ -18,78 +35,77 @@ class ProductListView extends StatefulWidget {
           padding: const EdgeInsets.all(10.0),
           child: Column(
             children: [
-              SizedBox(
-                height: 100.0,
-                child: Row(
-                  children: [
-                    Expanded(
+              Row(
+                children: [
+                  Expanded(
+                    child: SizedBox(
+                      height: 42,
                       child: SizedBox(
                         height: 42,
-                        child: SizedBox(
-                          height: 42,
-                          child: ElevatedButton(
-                            style: ElevatedButton.styleFrom(
-                                backgroundColor: Colors.black,
-                                foregroundColor: Colors.white,
-                                shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(8.0),
-                                )),
-                            onPressed: () {},
-                            child: Row(
-                              children: const [
-                                Expanded(
-                                  child: Text(
-                                    "Filter",
-                                  ),
-                                ),
-                                Icon(
-                                  Icons.tune,
-                                  size: 24.0,
-                                ),
-                              ],
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.black,
+                            foregroundColor: Colors.white,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(8.0),
                             ),
+                          ),
+                          onPressed: () {},
+                          child: Row(
+                            children: const [
+                              Expanded(
+                                child: Text(
+                                  "Sort By",
+                                ),
+                              ),
+                              Icon(
+                                Icons.sort,
+                                size: 24.0,
+                              ),
+                            ],
                           ),
                         ),
                       ),
                     ),
-                    const SizedBox(width: 12),
-                    Expanded(
+                  ),
+                  const SizedBox(width: 12),
+                  Expanded(
+                    child: SizedBox(
+                      height: 42,
                       child: SizedBox(
                         height: 42,
-                        child: SizedBox(
-                          height: 42,
-                          child: ElevatedButton(
-                            style: ElevatedButton.styleFrom(
-                                backgroundColor: Colors.black,
-                                foregroundColor: Colors.white,
-                                shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(8.0),
-                                )),
-                            onPressed: () {},
-                            child: Row(
-                              children: const [
-                                Expanded(
-                                  child: Text(
-                                    "Filter",
-                                  ),
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.black,
+                              foregroundColor: Colors.white,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(8.0),
+                              )),
+                          onPressed: () {},
+                          child: Row(
+                            children: const [
+                              Expanded(
+                                child: Text(
+                                  "Filter",
                                 ),
-                                Icon(
-                                  Icons.tune,
-                                  size: 24.0,
-                                ),
-                              ],
-                            ),
+                              ),
+                              Icon(
+                                Icons.tune,
+                                size: 24.0,
+                              ),
+                            ],
                           ),
                         ),
                       ),
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
+              const SizedBox(height: 20.0),
               GridView.builder(
                 padding: EdgeInsets.zero,
                 gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  childAspectRatio: 1.0 / 1.4,
+                  childAspectRatio: 1.0 / 1.6,
                   crossAxisCount: 2,
                   mainAxisSpacing: 8,
                   crossAxisSpacing: 8,
@@ -121,8 +137,24 @@ class ProductListView extends StatefulWidget {
                               ),
                             ),
                             child: Stack(
-                              children: const [
-                                Positioned(
+                              children: [
+                                Container(
+                                  decoration: BoxDecoration(
+                                    // color: Colors.black.withOpacity(.4),
+                                    gradient: LinearGradient(
+                                      begin: Alignment.bottomCenter,
+                                      end: Alignment.topCenter,
+                                      colors: [
+                                        Colors.black.withOpacity(.8),
+                                        Colors.black.withOpacity(.6),
+                                        Colors.black.withOpacity(.4),
+                                        Colors.black.withOpacity(.2),
+                                        Colors.black.withOpacity(.0),
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                                const Positioned(
                                   right: 6.0,
                                   top: 8.0,
                                   child: CircleAvatar(
@@ -135,6 +167,26 @@ class ProductListView extends StatefulWidget {
                                     ),
                                   ),
                                 ),
+                                SizedBox(
+                                  width: MediaQuery.of(context).size.width,
+                                  child: Column(
+                                    mainAxisAlignment: MainAxisAlignment.end,
+                                    children: const [
+                                      Text(
+                                        "text",
+                                        style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold, color: Colors.white),
+                                      ),
+                                      Text(
+                                        "text",
+                                        style: TextStyle(fontSize: 12.0, color: Colors.white),
+                                      ),
+                                      Text(
+                                        "text",
+                                        style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold, color: Colors.white),
+                                      ),
+                                    ],
+                                  ),
+                                )
                               ],
                             ),
                           ),
