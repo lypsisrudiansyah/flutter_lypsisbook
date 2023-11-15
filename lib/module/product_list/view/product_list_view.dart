44a23e609d4f245475d9459b18456f57116206e3
Rudiansyah <rudiansyahether@gmail.com>
2023-11-15 22:45:23 +0700

    🔨 refactor(product_list_view.dart): improve code formatting and structure for better readability and maintainability 🎨 style(product_list_view.dart): update indentation and spacing to follow Flutter style guidelines

--- lib/module/product_list/view/product_list_view.dart
+++ lib/module/product_list/view/product_list_view.dart
@@ -1,31 +1,185 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter_lypsisbook/core.dart';
 import '../controller/product_list_controller.dart';
 
 class ProductListView extends StatefulWidget {
-    const ProductListView({Key? key}) : super(key: key);
+  const ProductListView({Key? key}) : super(key: key);
 
-    Widget build(context, ProductListController controller) {
+  Widget build(context, ProductListController controller) {
     controller.view = this;
 
     return Scaffold(
-        appBar: AppBar(
+      appBar: AppBar(
         title: const Text("ProductList"),
         actions: const [],
-        ),
-        body: SingleChildScrollView(
+      ),
+      body: SingleChildScrollView(
         child: Container(
-            padding: const EdgeInsets.all(10.0),
-            child: Column(
-            children: const [],
-            ),
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+            children: [
+              SizedBox(
+                height: 100.0,
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: SizedBox(
+                        height: 42,
+                        child: SizedBox(
+                          height: 42,
+                          child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                                backgroundColor: Colors.black,
+                                foregroundColor: Colors.white,
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(8.0),
+                                )),
+                            onPressed: () {},
+                            child: Row(
+                              children: const [
+                                Expanded(
+                                  child: Text(
+                                    "Filter",
+                                  ),
+                                ),
+                                Icon(
+                                  Icons.tune,
+                                  size: 24.0,
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(width: 12),
+                    Expanded(
+                      child: SizedBox(
+                        height: 42,
+                        child: SizedBox(
+                          height: 42,
+                          child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                                backgroundColor: Colors.black,
+                                foregroundColor: Colors.white,
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(8.0),
+                                )),
+                            onPressed: () {},
+                            child: Row(
+                              children: const [
+                                Expanded(
+                                  child: Text(
+                                    "Filter",
+                                  ),
+                                ),
+                                Icon(
+                                  Icons.tune,
+                                  size: 24.0,
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              GridView.builder(
+                padding: EdgeInsets.zero,
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  childAspectRatio: 1.0 / 1.4,
+                  crossAxisCount: 2,
+                  mainAxisSpacing: 8,
+                  crossAxisSpacing: 8,
+                ),
+                itemCount: controller.products.length,
+                shrinkWrap: true,
+                physics: const ScrollPhysics(),
+                itemBuilder: (BuildContext context, int index) {
+                  var item = controller.products[index];
+                  return Container(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Expanded(
+                          child: Container(
+                            width: MediaQuery.of(context).size.width,
+                            clipBehavior: Clip.antiAlias,
+                            decoration: BoxDecoration(
+                              image: DecorationImage(
+                                image: NetworkImage(
+                                  item["photo"],
+                                ),
+                                fit: BoxFit.cover,
+                              ),
+                              borderRadius: const BorderRadius.all(
+                                Radius.circular(
+                                  6.0,
+                                ),
+                              ),
+                            ),
+                            child: Stack(
+                              children: const [
+                                Positioned(
+                                  right: 6.0,
+                                  top: 8.0,
+                                  child: CircleAvatar(
+                                    radius: 14.0,
+                                    backgroundColor: Colors.white,
+                                    child: Icon(
+                                      Icons.favorite,
+                                      color: Colors.red,
+                                      size: 14.0,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 8.0,
+                        ),
+                        Text(
+                          item["product_name"],
+                          style: const TextStyle(
+                            fontSize: 16.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 4.0,
+                        ),
+                        Text(
+                          item["category"],
+                          style: const TextStyle(
+                            fontSize: 12.0,
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 4.0,
+                        ),
+                        Text(
+                          "${item["price"]}",
+                          style: const TextStyle(
+                            fontSize: 16.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                  );
+                },
+              ),
+            ],
+          ),
         ),
-        ),
+      ),
     );
-    }
+  }
 
-    @override
-    State<ProductListView> createState() => ProductListController();
+  @override
+  State<ProductListView> createState() => ProductListController();
 }
-    
