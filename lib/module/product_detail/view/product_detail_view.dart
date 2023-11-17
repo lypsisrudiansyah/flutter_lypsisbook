bb58e936df8eb6f4bf23c35ef65ee0098417405e
Rudiansyah <rudiansyahether@gmail.com>
2023-11-17 22:08:59 +0700

    🔀 chore(main.dart): update home screen to display ProductDetailView instead of MainNavigationView for testing purposes 🔀 chore(product_detail_view.dart): update app bar to have transparent background and add favorite icon button

--- lib/module/product_detail/view/product_detail_view.dart
+++ lib/module/product_detail/view/product_detail_view.dart
@@ -1,31 +1,48 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter_lypsisbook/core.dart';
 import '../controller/product_detail_controller.dart';
 
 class ProductDetailView extends StatefulWidget {
-    const ProductDetailView({Key? key}) : super(key: key);
+  const ProductDetailView({Key? key}) : super(key: key);
 
-    Widget build(context, ProductDetailController controller) {
+  Widget build(context, ProductDetailController controller) {
     controller.view = this;
 
     return Scaffold(
-        appBar: AppBar(
-        title: const Text("ProductDetail"),
-        actions: const [],
-        ),
-        body: SingleChildScrollView(
-        child: Container(
-            padding: const EdgeInsets.all(10.0),
-            child: Column(
-            children: const [],
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        actions: const [
+          CircleAvatar(
+            backgroundColor: Colors.white,
+            child: Icon(
+              Icons.favorite,
+              color: Colors.red,
             ),
+          ),
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: 160.0,
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: NetworkImage(
+                    "https://images.unsplash.com/photo-1533050487297-09b450131914?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+                  ),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+          ],
         ),
-        ),
+      ),
     );
-    }
+  }
 
-    @override
-    State<ProductDetailView> createState() => ProductDetailController();
+  @override
+  State<ProductDetailView> createState() => ProductDetailController();
 }
-    
