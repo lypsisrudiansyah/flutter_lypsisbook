69a2781a284c2adef2d9d12bf045c30212b33ceb
Rudiansyah <rudiansyahether@gmail.com>
2023-12-17 22:29:06 +0700

    ✨ feat(profile_controller.dart): add menuItems list to store the profile menu items ✨ feat(profile_view.dart): add CircleAvatar widget to display user profile picture ✨ feat(profile_view.dart): add Text widgets to display user name and job title ✨ feat(profile_view.dart): add ListTile widgets to display other information and menu items

--- lib/module/profile/controller/profile_controller.dart
+++ lib/module/profile/controller/profile_controller.dart
@@ -1,23 +1,54 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter_lypsisbook/core.dart';
-import '../view/profile_view.dart';
 
 class ProfileController extends State<ProfileView> {
-    static late ProfileController instance;
-    late ProfileView view;
+  static late ProfileController instance;
+  late ProfileView view;
 
-    @override
-    void initState() {
-        instance = this;
-        super.initState();
-    }
+  @override
+  void initState() {
+    instance = this;
+    super.initState();
+  }
 
-    @override
-    void dispose() => super.dispose();
+  @override
+  void dispose() => super.dispose();
 
-    @override
-    Widget build(BuildContext context) => widget.build(context, this);
+  @override
+  Widget build(BuildContext context) => widget.build(context, this);
+
+  List menuItems = [
+    {
+      "label": "Edit Profile",
+      "view": const EditProfileView(),
+    },
+    {
+      "label": "Change Password",
+      "view": const ChangePasswordView(),
+    },
+    {
+      "label": "Settings",
+      "view": const EditProfileView(),
+    },
+    {
+      "label": "Help",
+      "view": const EditProfileView(),
+    },
+    {
+      "label": "FAQ",
+      "view": const EditProfileView(),
+    },
+    {
+      "label": "Privacy Policy",
+      "view": const EditProfileView(),
+    },
+    {
+      "label": "TOS",
+      "view": const EditProfileView(),
+    },
+    {
+      "label": "Logout",
+      "view": const EditProfileView(),
+    },
+  ];
 }
-        
-    
--- lib/module/profile/view/profile_view.dart
+++ lib/module/profile/view/profile_view.dart
@@ -6,24 +6,66 @@ class ProfileView extends StatefulWidget {
 
   Widget build(context, ProfileController controller) {
     controller.view = this;
+    var mqH = MediaQuery.of(context).size.height;
+    var mqW = MediaQuery.of(context).size.width;
 
     return Scaffold(
       appBar: AppBar(
         title: const Text("Profile"),
         actions: const [],
+        centerTitle: true,
       ),
       body: SingleChildScrollView(
         child: Container(
-          padding: const EdgeInsets.all(10.0),
+          width: mqW,
+          padding: const EdgeInsets.all(20.0),
           child: Column(
             children: [
-              ListTile(
-                title: const Text("Change Password"),
-                trailing: const Icon(
-                  Icons.chevron_right,
-                  size: 24.0,
+              const CircleAvatar(
+                radius: 64.0,
+                backgroundImage: NetworkImage(
+                  "https://i.ibb.co/PGv8ZzG/me.jpg",
                 ),
-                onTap: () => Get.to(const ChangePasswordView()),
+              ),
+              const SizedBox(height: 4.0),
+              const Text(
+                "Rudiansyah",
+                style: TextStyle(
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 4.0),
+              const Text(
+                "Software Engineer at Xyz",
+                style: TextStyle(
+                  fontSize: 14.0,
+                ),
+              ),
+              const SizedBox(
+                height: 20.0,
+              ),
+              const ListTile(
+                contentPadding: EdgeInsets.zero,
+                title: Text("OTHER INFORMATION"),
+              ),
+              ListView.builder(
+                itemCount: controller.menuItems.length,
+                shrinkWrap: true,
+                physics: const ScrollPhysics(),
+                itemBuilder: (BuildContext context, int index) {
+                  var itemMenu = controller.menuItems[index];
+
+                  return ListTile(
+                    contentPadding: EdgeInsets.zero,
+                    title: Text(itemMenu['label']),
+                    trailing: const Icon(
+                      Icons.chevron_right,
+                      size: 24.0,
+                    ),
+                    onTap: () => Get.to(itemMenu['view']),
+                  );
+                },
               ),
             ],
           ),
