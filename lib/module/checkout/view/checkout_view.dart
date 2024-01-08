51ad485fbafe394951fa224ead53c5acdb89763d
Rudiansyah <rudiansyahether@gmail.com>
2024-01-08 23:57:15 +0700

    🔧 fix(checkout_view.dart): fix indentation and remove unused import statement 🚀 feat(checkout_view.dart): add support for displaying cart items in the checkout view

--- lib/module/checkout/view/checkout_view.dart
+++ lib/module/checkout/view/checkout_view.dart
@@ -1,24 +1,47 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter_lypsisbook/core.dart';
-import '../controller/checkout_controller.dart';
 
 class CheckoutView extends StatefulWidget {
   const CheckoutView({Key? key}) : super(key: key);
 
   Widget build(context, CheckoutController controller) {
-  controller.view = this;
+    controller.view = this;
+    final cartController = CartController.instance;
+
     return Scaffold(
       appBar: AppBar(
         title: const Text("Checkout"),
         actions: const [],
-        ),
-        body: SingleChildScrollView(
-        child: Container(
-            padding: const EdgeInsets.all(10.0),
-            child: Column(
-            children: const [],
+      ),
+      body: Container(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(
+          children: [
+            const ListTile(
+              contentPadding: EdgeInsets.zero,
+              title: Text("Cart Items"),
             ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: 3,
+                physics: const ScrollPhysics(),
+                itemBuilder: (BuildContext context, int index) {
+                  return Card(
+                    child: ListTile(
+                      leading: CircleAvatar(
+                        backgroundColor: Colors.grey[200],
+                        backgroundImage: const NetworkImage(
+                          "https://i.ibb.co/QrTHd59/woman.jpg",
+                        ),
+                      ),
+                      title: const Text("Jessica Doe"),
+                      subtitle: const Text("Programmer"),
+                    ),
+                  );
+                },
+              ),
+            )
+          ],
         ),
       ),
     );
@@ -27,4 +50,3 @@ class CheckoutView extends StatefulWidget {
   @override
   State<CheckoutView> createState() => CheckoutController();
 }
-    
