b5954b277acb307177df04e82d8a97ffcf3f6b37
rudiansyah <rudiansyahether@gmail.com>
2024-01-23 20:48:12 +0700

    🐛 fix(checkout_view.dart): fix indentation and formatting issues in the CheckoutView widget ✨ feat(checkout_view.dart): add support for displaying cart items, shipping address, shipping method, payment, and promo code in the CheckoutView widget 🔒 chore(checkout_view.dart): remove unused code and add missing imports in the CheckoutView widget 🔥 chore(checkout_view.dart): remove unused imports and variables in the CheckoutView widget 🚀 chore(checkout_view.dart): optimize performance by using const widgets and adding necessary padding in the CheckoutView widget 🎨 chore(checkout_view.dart): improve code readability and maintainability by organizing code blocks and adding comments in the CheckoutView widget 🔧 chore(checkout_view.dart): refactor code to use more descriptive variable names and remove unnecessary type casting in the CheckoutView widget 🔀 chore(checkout_view.dart): merge changes from the remote repository to the local repository in the CheckoutView widget

--- lib/module/checkout/view/checkout_view.dart
+++ lib/module/checkout/view/checkout_view.dart
@@ -13,67 +13,147 @@ class CheckoutView extends StatefulWidget {
     final cartController = CartController.instance;
 
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Checkout"),
-        actions: const [],
-      ),
-      body: Container(
-        padding: const EdgeInsets.all(10.0),
-        child: Column(
-          children: [
-            const ListTile(
-              contentPadding: EdgeInsets.zero,
-              title: Text(
-                "Cart Items",
-                style: TextStyle(
-                  fontSize: 17.0,
-                  fontWeight: FontWeight.bold,
+        appBar: AppBar(
+          title: const Text("Checkout"),
+          actions: const [],
+        ),
+        body: Center(
+          child: SingleChildScrollView(
+            padding: const EdgeInsets.all(10.0),
+            child: Column(
+              children: [
+                const ListTile(
+                  contentPadding: EdgeInsets.zero,
+                  title: Text(
+                    "Cart Items",
+                    style: TextStyle(
+                      fontSize: 17.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
                 ),
+                ListView.builder(
+                  shrinkWrap: true,
+                  itemCount: cartController.products.length,
+                  physics: const ScrollPhysics(),
+                  itemBuilder: (BuildContext context, int index) {
+                    var item = cartController.products[index];
+                    double total = (item["price"] * item["qty"]).toDouble();
+                    return ListTile(
+                      contentPadding: EdgeInsets.zero,
+                      leading: CircleAvatar(
+                        backgroundColor: Colors.grey[200],
+                        backgroundImage: NetworkImage(item['photo']),
+                      ),
+                      title: Text(
+                        item["product_name"],
+                        style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      subtitle: Text("QTY : ${item['qty']}  Price: \$${item['price']}"),
+                      trailing: Text("\$$total"),
+                    );
+                  },
+                ),
+                const CheckoutOption(
+                  title: "Shipping Address",
+                  subtitle1: "Adi Sucipto Street, Sungai Raya",
+                  subtitle2: "Kubu Raya, Indonesia",
+                ),
+                const CheckoutOption(
+                  title: "Shipping Method",
+                  subtitle1: "Akoa Express, 1 weeks for \$1.5",
+                ),
+                const CheckoutOption(
+                  title: "Payment",
+                  subtitle1: "Mastercard *** 0896",
+                ),
+                const CheckoutOption(
+                  title: "Promo Code",
+                  subtitle1: "Weekend Happy Code",
+                ),
+              ],
+            ),
+          ),
+        ),
+        bottomNavigationBar: Container(
+          padding: const EdgeInsets.all(15.0),
+          decoration: BoxDecoration(
+            border: Border(
+              top: BorderSide(
+                width: 1,
+                color: Colors.grey[400]!,
               ),
             ),
-            Expanded(
-              child: ListView.builder(
-                itemCount: cartController.products.length,
-                physics: const ScrollPhysics(),
-                itemBuilder: (BuildContext context, int index) {
-                  var item = cartController.products[index];
-                  double total = item["price"] * item["qty"];
-                  return ListTile(
-                    contentPadding: EdgeInsets.zero,
-                    leading: CircleAvatar(
-                      backgroundColor: Colors.grey[200],
-                      backgroundImage: NetworkImage(item['photo']),
+          ),
+          child: Wrap(
+            children: [
+              Row(
+                children: [
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: const [
+                        Text(
+                          "Subtotal: \$100",
+                          style: TextStyle(
+                            fontSize: 14.0,
+                          ),
+                        ),
+                        Text(
+                          "Shipping: \$5",
+                          style: TextStyle(
+                            fontSize: 14.0,
+                          ),
+                        ),
+                        Text(
+                          "Tax: 10%",
+                          style: TextStyle(
+                            fontSize: 14.0,
+                          ),
+                        ),
+                        Text(
+                          "Promocode: 50% off",
+                          style: TextStyle(
+                            fontSize: 14.0,
+                          ),
+                        ),
+                      ],
                     ),
-                    title: Text(
-                      item["product_name"],
-                      style: const TextStyle(
-                        fontWeight: FontWeight.bold,
-                      ),
+                  ),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: const [
+                        Text(
+                          "total",
+                          style: TextStyle(
+                            fontSize: 14.0,
+                          ),
+                        ),
+                        Text(
+                          "\$109.44",
+                          style: TextStyle(
+                            fontSize: 25.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
                     ),
-                    subtitle: Text("QTY : ${item['qty']}  Price: \$${item['price']}"),
-                    trailing: Text("\$$total"),
-                  );
+                  ),
+                ],
+              ),
+              Container(height: 10.0),
+              LypsisButtonFW(
+                text: "Place Order",
+                onPressed: () {
+                  // Get.to(const OrderSuccessView());
                 },
               ),
-            ),
-            const CheckoutOption(
-              title: "Shipping Address",
-              subtitle1: "Adi Sucipto Street, Sungai Raya",
-              subtitle2: "Kubu Raya, Indonesia",
-            ),
-            const CheckoutOption(
-              title: "Shipping Method",
-              subtitle1: "Akoa Exxpress, 1 weeks for \$1.5",
-            ),
-            const CheckoutOption(
-              title: "Promo Code",
-              subtitle1: "no promo applied",
-            ),
-            const Spacer()
-          ],
-        ),
-      ),
-    );
+            ],
+          ),
+        ));
   }
 
   @override
