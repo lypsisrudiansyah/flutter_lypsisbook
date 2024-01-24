d3345262c837de50d65bed3e90ddc1139afdd960
rudiansyah <rudiansyahether@gmail.com>
2024-01-24 20:20:48 +0700

    📦 feat(order_list_delivered_items.dart): add OrderListDeliveredItems widget to display delivered items in the order list 📦 feat(order_list_shipped_items.dart): add OrderListShippedItems widget to display shipped items in the order list
    These two new files introduce two new widgets, `OrderListDeliveredItems` and `OrderListShippedItems`, which are used to display the list of delivered and shipped items respectively in the order list. These widgets are responsible for rendering the UI for each item in the list, including the item ID, creation date, payment method, courier, and price. The items are passed to these widgets as a list of maps, where each map represents an item with its corresponding data. The widgets use the `ListView.builder` to efficiently render the list of items.

---
+++ lib/module/order_list/widget/order_list_delivered_items.dart
@@ -0,0 +1,79 @@
+// ignore_for_file: camel_case_types, prefer_typing_uninitialized_variables
+
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class OrderListDeliveredItems extends StatelessWidget {
+  final List<Map<String, dynamic>> items;
+
+  const OrderListDeliveredItems({Key? key, required this.items}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: items.length,
+      physics: const ScrollPhysics(),
+      itemBuilder: (BuildContext context, int index) {
+        var item = items[index];
+
+        return Container(
+          padding: const EdgeInsets.all(15.0),
+          decoration: BoxDecoration(
+            border: Border(
+              bottom: BorderSide(
+                color: Colors.grey[300]!,
+              ),
+            ),
+          ),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "#${item["id"]}",
+                          style: const TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16.0,
+                          ),
+                        ),
+                        Text(
+                          DateFormat('dd MMMM y kk:mm').format(DateTime.parse(item['created_at'])),
+                          style: const TextStyle(
+                            fontSize: 13.0,
+                          ),
+                        ),
+                        Text(
+                          "${item['payment_method']['name'] ?? '-'}",
+                          style: const TextStyle(
+                            fontSize: 13.0,
+                          ),
+                        ),
+                        Text(
+                          "${item['courier']['name'] ?? '-'}",
+                          style: const TextStyle(
+                            fontSize: 13.0,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const Text(
+                    "\$213",
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
